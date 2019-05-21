8143ea71b85344ed66f0291661f09f45ae43389f
Nadia Morris <reversegremlin@gmail.com>
2019-05-20 21:45:14 -0400

    building base world, enemy

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -24,6 +24,9 @@ public class CubeEditor : MonoBehaviour
 
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
 
-        textMesh.text = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+        string labelText = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+        textMesh.text = labelText;
+        gameObject.name = "cube " + labelText;
     }
+
 }
