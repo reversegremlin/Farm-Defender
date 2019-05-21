43378172cfe29e71f3ad5f707d3c93532e8ac441
Nadia Morris <reversegremlin@gmail.com>
2019-05-20 21:59:19 -0400

    Enemy Mover Script started, path is stored as a List of Blocks. Recolored Cube prefab

---
+++ Assets/EnemyMover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+
+    [SerializeField] List<Block> enemyPath;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Block cube in enemyPath)
+        {
+            print(cube.name);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
