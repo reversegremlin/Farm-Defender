e379b51ec3a0fb192e84f8f02c5135aeb3ae109b
Nadia Morris <reversegremlin@gmail.com>
2019-05-20 22:44:41 -0400

    Enemy path printing.

--- Assets/EnemyMover.cs
+++ Assets/EnemyMover.cs
@@ -6,16 +6,27 @@ public class EnemyMover : MonoBehaviour
 {
 
     [SerializeField] List<Block> enemyPath;
+    [SerializeField] float dwellTime = 1.0f;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine(FollowPath());
+    }
+
+    IEnumerator FollowPath()
+    {
         foreach (Block cube in enemyPath)
         {
-            print(cube.name);
+            Vector3 pos = new Vector3(
+            cube.transform.position.x + 15f,
+            cube.transform.position.y + 6f,
+            cube.transform.position.z
+            );
+            gameObject.transform.position = pos;
+            yield return new WaitForSeconds(dwellTime);
         }
-
     }
 
     // Update is called once per frame
