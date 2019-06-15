1d20fa568f4fcdeafd3c3032b54961bca1f7b788
Nadia Morris <reversegremlin@gmail.com>
2019-06-15 18:04:41 -0400

    Added tower pan script.

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+
+    [SerializeField] Transform objectToPan;
+    [SerializeField] Transform targetEnemy;
+
+
+    //public class ExampleClass : MonoBehaviour
+    //{
+    //    public Transform target;
+
+    //    void Update()
+    //    {
+    //        // Rotate the camera every frame so it keeps looking at the target
+    //        transform.LookAt(target);
+    //    }
+    //}
+
+    // Start is called before the first frame update
+    // Update is called once per frame
+    void Update()
+    {
+        objectToPan.LookAt(targetEnemy);
+    }
+}
