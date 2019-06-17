99dca14016f125d0c348f0428c0ac79ff78212ba
Nadia Morris <reversegremlin@gmail.com>
2019-06-16 20:32:50 -0400

    Collision handler, enemy health implemented.

---
+++ Assets/Scripts/EnemyCollider.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCollider : MonoBehaviour
+{
+
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 100;
+    [SerializeField] int hits = 10;
+
+
+  //  ScoreBoard scoreBoard;
+
+    void Start()
+    {
+        AddNonTriggerBoxCollider();
+     //   scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+    private void AddNonTriggerBoxCollider()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        print("Hits: " + hits);
+
+        --hits;
+        if (hits <= 1)
+        //todo: consider hit effect
+        {
+            KillEnemy();
+        }
+    }
+
+    private void KillEnemy()
+    {
+     //   scoreBoard.ScoreHit(scorePerHit);
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScorebBoard : MonoBehaviour
+{
+
+    int score = 0;
+    Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        scoreText.text = score.ToString();
+    }
+
+    public void ScoreHit(int enemyValue)
+    {
+        score = score + enemyValue;
+        print(score);
+        scoreText.text = score.ToString();
+    }
+}
