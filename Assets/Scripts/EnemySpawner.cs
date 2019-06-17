684cc60d541f69bc4644ffe6ded3675d0bf28c54
Nadia Morris <reversegremlin@gmail.com>
2019-06-17 14:00:59 -0400

    Adding enemy spawner

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    [SerializeField] float secondsBetweenSpawns = 3.0f;
+    [SerializeField] EnemyMover enemyToSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
